bd6d90ac0f68b31baa4cfa7b30f6d1e440514b38
MihiroHashimoto <mihiro.Hashimoto.1125@gmail.com>
2020-07-31 23:56:49 +0900

    Add: チャット メッセージ横にに送信者も表示

--- app/Message.php
+++ app/Message.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     protected $guarded = ['id'];
+
+    /**
+     * メッセージを所有するUserを取得
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
