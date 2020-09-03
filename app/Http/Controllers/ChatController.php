cb679b935428feb041699de5f9f49a3e8cf621f1
MihiroHashimoto <mihiro.Hashimoto.1125@gmail.com>
2020-09-03 09:02:12 +0900

    Add: 動画もアップロードできるようにした

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -18,10 +18,24 @@ class ChatController extends Controller
 
     public function create(Request $request)
     {
+        $inputs = $request->validate([
+            'user_id' => 'required|integer',
+            // 'photo' => 'nullable|file|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'photo' => 'nullable|file|mimes:jpeg,png,jpg,gif,mp4,mp4v,mpg4,3gp|max:51200',
+            'message' => 'required|string'
+        ]);
+
+        $image_path = '';
+        if (isset($request->photo)) {
+            $store_path = $request->photo->store('public/points_images');
+            $image_path = basename($store_path);
+        }
         $message = \App\Message::create([
             'user_id' => $request->user_id,
+            'image_path' => $image_path,
             'body' => $request->message
         ]);
         event(new MessageCreated($message));
+        return response()->json(['message' => $image_path], 200);
     }
 }
